24251e57195dd7d904c681fd7243662219c59997
Leael95 <leaxplosion@gmail.com>
2022-08-10 20:52:43 -0300

    add: main board cell randomizer

--- gameoflife.rb
+++ gameoflife.rb
@@ -11,12 +11,28 @@ class Gameoflife
 
   def main
     create_table
+    cells_randomizer(@gamedimensions)
     show_table
   end
 
   def show_table
     for a in 0..@height-1 do
       puts @gamedimensions[a].join(' ')
+    end
+  end
+
+  def cells_randomizer(main_game)
+    # Ciclo de la altura para poder randomizar celulas vivas
+    for b in 0..@height-1
+      # Ciclo de la anchura para poder randomizar celulas vivas
+      for c in 0..@width-1
+        # @number va a generar un numero aleatorio con la funcion rand, si sale 1, entonces en la ubicacion del loop va a ingresar una celula viva
+        @number = rand(1..5)
+
+        if @number == 1
+          main_game[b][c] = "*"
+        end
+      end
     end
   end
 
